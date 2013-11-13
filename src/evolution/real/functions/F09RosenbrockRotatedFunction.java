e5fb3d4c3eea07b5744cc3f3e6ac439450aeaf02
Martin Pilat <martin.pilat@gmail.com>
2013-11-13 01:09:23 +0100

    Fixed in bug in F09. (does not have xopt set)

--- src/evolution/real/functions/F09RosenbrockRotatedFunction.java
+++ src/evolution/real/functions/F09RosenbrockRotatedFunction.java
@@ -20,6 +20,7 @@ public class F09RosenbrockRotatedFunction extends RealFunction {
 
     @Override
     public void reinit() {
+        xopt = new double[D];
         double g1 = RandomNumberGenerator.getInstance().nextGaussian();
         double g2 = RandomNumberGenerator.getInstance().nextGaussian();
         fopt = Math.min(1000.0, Math.max(-1000.0, (Math.round(100.0 * 100.0 * g1 / g2) / 100.0)));
