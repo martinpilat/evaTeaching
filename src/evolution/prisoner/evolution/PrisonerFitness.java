8171787d3ef30b88399b3e7e7042b6fd158174c6
Martin Pilat <martin.pilat@gmail.com>
2014-11-12 11:35:00 +0100

    Fixed a small bug in the computation of fitness in Prisoner's dilemma strategy evolution.
    .reset() is now called before each strategy is executed

--- src/evolution/prisoner/evolution/PrisonerFitness.java
+++ src/evolution/prisoner/evolution/PrisonerFitness.java
@@ -40,11 +40,9 @@ public class PrisonerFitness implements FitnessFunction {
 
         for (Strategy s : strategies) {
 
-            int sc1 = 0;
-            int sc2 = 0;
+            s.reset();
 
-            String str1 = "";
-            String str2 = "";
+            int sc1 = 0;
 
             int iters = 150 + rnd.nextInt(100);
 
@@ -56,14 +54,11 @@ public class PrisonerFitness implements FitnessFunction {
                 s1Move = es.nextMove();
                 s2Move = s.nextMove();
 
-                str1 += (s1Move == null) ? "E" : s1Move.getLabel();
-                str2 += (s2Move == null) ? "E" : s2Move.getLabel();
 
                 Result r1 = new Result(s1Move, s2Move);
                 Result r2 = new Result(s2Move, s1Move);
 
                 sc1 += r1.getMyScore();
-                sc2 += r2.getMyScore();
 
                 try {
                     es.reward(r1);
@@ -73,7 +68,6 @@ public class PrisonerFitness implements FitnessFunction {
                 try {
                     s.reward(r2);
                 } catch (Exception e) {
-                    sc2 = 0;
                 }
 
             }
